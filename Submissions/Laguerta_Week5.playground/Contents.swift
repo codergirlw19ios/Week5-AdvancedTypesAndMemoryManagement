6d00e90f20109f16b22080bea17175b8ea312b44
SLaguerta <slaguerta@alumni.nd.edu>
2019-03-16 21:38:29 -0500

    Edit problem 2 to reach third catch block

--- Submissions/Laguerta_Week5.playground/Contents.swift
+++ Submissions/Laguerta_Week5.playground/Contents.swift
@@ -7,6 +7,10 @@ enum PrinterError: Error {
 
 }
 
+enum ConnectError: Error {
+    case connect
+}
+
 func send(job: Int, toPrinter printerName: String) throws -> String {
     if printerName == "Never Has Toner" {
         throw PrinterError.noToner
@@ -31,14 +35,14 @@ do {
     let printerResponse = try send(job: 1440, toPrinter: "Gutenberg")
     //let error = "Can't seem to find this printer"
     //throw PrinterError.onFire --to trigger first catch block
-    throw PrinterError.outOfPaper //to throw second catch block
-    
+    //throw PrinterError.outOfPaper //to throw second catch block
+    throw ConnectError.connect //for third catch block, I create a different Error enum 
 } catch PrinterError.onFire {
     print("I'll just put this over here, with the rest of the fire.")
 } catch let printerError as PrinterError {
     print("Printer error: \(printerError).")
 } catch {
-    print(error)
+    print("Can't seem to find this printer!")
 }
 //:
 //:
